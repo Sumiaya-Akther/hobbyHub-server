65b41a190195c5d1ef5b7913368f5031bf1ac859
Sumiaya Akther <sumiayaakther15@gmail.com>
2025-05-24 01:29:30 +0600

    add user

--- index.js
+++ index.js
@@ -14,8 +14,6 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 // const uri = "mongodb+srv://<db_username>:<db_password>@cluster0.mdnalzr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 
-
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -25,11 +23,52 @@ const client = new MongoClient(uri, {
 });
 async function run() {
   try {
-
-
-
-    
     await client.connect();
+
+
+    const groupsCollection = client.db('groupDB').collection('groups');
+    const userCollection = client.db('groupDB').collection('users');
+
+
+
+    // app.get('/new-groups', async (req, res) => {
+    //         const today = new Date();
+    //         const result = await groupCollection.find({startDate: { $gte: today.toISOString()}})
+    //         .limit(6)
+    //         .toArray();
+    //         res.send(result);
+    //     })
+
+    app.get('/groups', async(req, res)=>{
+        //   const cursor = groupsCollection.find();
+        //   const result = await cursor.toArray();
+        const result = await groupsCollection.find().toArray();
+        res.send(result);
+    })
+    
+    app.post('/groups', async(req, res) => {
+         
+        const newGroup = req.body;
+        console.log(newGroup);
+        const result = await groupsCollection.insertOne(newGroup);
+        res.send(result);
+
+    })
+
+    //user related api
+    
+    app.post('/users', async(req, res) => {
+        const userProfile = req.body;
+        console.log(userProfile);
+        const result = await userCollection.insertOne(userProfile);
+        res.send(result);
+    })
+
+
+
+
+
+
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
