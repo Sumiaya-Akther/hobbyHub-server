f4ea7b56d4c691b3294841e2e3487ac5048dfdde
Sumiaya Akther <sumiayaakther15@gmail.com>
2025-05-24 23:13:40 +0600

    new

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const client = new MongoClient(uri, {
 });
 async function run() {
   try {
-    await client.connect();
+   // await client.connect();
 
 
     const groupsCollection = client.db('groupDB').collection('groups');
